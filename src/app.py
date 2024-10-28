39482a5ec98e59b60b7a6dcc6743b3b61a4380ae
IEVN1001-21000502 <joshua200314@gmail.com>
2024-10-28 09:06:46 -0600

    Commit 2

--- src/app.py
+++ src/app.py
@@ -1,36 +1,48 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
+from flask_mysqldb import MySQL
 
-app =Flask(__name__)
+from config import config
+
+app = Flask(__name__)
+
+con = MySQL(app)
 
 @app.route("/")
 def index():
-    titulo =  "IEVN-1001"
-    list = ['josh', 'dany', 'prueba']
-    return render_template("uno.html", titulo= titulo, list=list)
-
-@app.route("/user/<string:user>")
-def user(user):
-    return "el usuario es {}".format(user)
-
-@app.route("/numero/<int:n1>")
-def numero(n1):
-    return "el numero es {}".format(n1)
+    return "hola"
 
 
-@app.route("/user/<string:nom>/<int:id>")
-def user2(nom, id):
-    return "<h1> ID:{} Nombre: {}</h1>".format(id, nom)
-
-@app.route("/suma/<int:n1>/<int:n2>")
-def suma(n1, n2):
-    return "<h1> la suma es: {}</h1>".format(n1 + n2)
-    
-@app.route("/default")
-@app.route("/default/<string:nom>")
-def nom2(nom = "KDS"):
-    return "<h1>el nombre es {}</h1>".format(nom)
+@app.route("/alumnos/<mat>", methods = ["GET"])
+def lista_alumnos(mat):
+    try:
+        alumno = leer_alumno_bd(mat)
+        if alumno is None:
+            return jsonify({'alumno': None, 'mensaje': 'Alumno no encontrado', 'exito': False}), 404
+        else:
+            return jsonify({'alumno': alumno, 'mensaje': 'Alumno encontrado', 'exito': True})
+    except Exception as ex:
+        return jsonify({'message': "error {}".format(ex), 'exito': False}), 500
 
 
-if __name__=="__main__":
-    app.run(debug=True)
-    
+def leer_alumno_bd(matricula):
+    try:
+        cursor = con.connection.cursor()
+        sql = 'SELECT * FROM alumnos WHERE matricula = %s'
+        cursor.execute(sql, [matricula])
+        datos = cursor.fetchone()
+        if datos is not None:
+            alumno = {"matricula": datos[0], "nombre": datos[1], "apaterno": datos[2], "amaterno": datos[3], "correo": datos[4]}
+            return alumno
+        else:
+            return None
+    except Exception as ex:
+        raise Exception("Error al leer alumno: {}".format(ex))
+
+
+def pagina_no_encontrada(error):
+    return "<h1>PAGINA NO ENCONTRADA 404</h1>", 404
+
+if __name__ == "__main__":
+    app.config.from_object(config['development'])
+    app.register_error_handler(404, pagina_no_encontrada)
+    app.run(host='0.0.0.0', port=5000)
