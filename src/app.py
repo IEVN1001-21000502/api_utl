df82b66317262e98e57b8fe2e4a9ae8e940e51d3
IEVN1001-21000502 <joshua200314@gmail.com>
2024-10-29 08:54:00 -0600

    Insert

--- src/app.py
+++ src/app.py
@@ -1,48 +1,83 @@
-from flask import Flask, render_template, request, jsonify
+from flask import Flask, request, jsonify
 from flask_mysqldb import MySQL
-
 from config import config
-
+ 
 app = Flask(__name__)
-
+ 
 con = MySQL(app)
-
-@app.route("/")
-def index():
-    return "hola"
-
-
-@app.route("/alumnos/<mat>", methods = ["GET"])
-def lista_alumnos(mat):
+ 
+@app.route('/alumnos', methods=['GET'])
+def lista_alumnos():
     try:
-        alumno = leer_alumno_bd(mat)
-        if alumno is None:
-            return jsonify({'alumno': None, 'mensaje': 'Alumno no encontrado', 'exito': False}), 404
-        else:
-            return jsonify({'alumno': alumno, 'mensaje': 'Alumno encontrado', 'exito': True})
+        cursor = con.connection.cursor()
+        sql="SELECT * FROM alumnos"
+        cursor.execute(sql)
+        datos = cursor.fetchall()
+        alumnos = []
+        for fila in datos:
+            alumno = {
+                'id': fila[0],
+                'nombre': fila[1],
+                'apellido': fila[2],
+                'email': fila[3]
+            }
+            alumnos.append(alumno)
+        return jsonify({'alumnos': alumnos, 'mensaje': 'Lista de alumnos'}), 200
     except Exception as ex:
-        return jsonify({'message': "error {}".format(ex), 'exito': False}), 500
-
-
+        return jsonify({"message": "error{}".format(ex), "exito": False}), 500
+    return ''
+ 
 def leer_alumno_bd(matricula):
     try:
         cursor = con.connection.cursor()
-        sql = 'SELECT * FROM alumnos WHERE matricula = %s'
-        cursor.execute(sql, [matricula])
+        sql = "SELECT * FROM alumnos WHERE matricula = {}".format(matricula)
+        cursor.execute(sql)
         datos = cursor.fetchone()
+       
         if datos is not None:
-            alumno = {"matricula": datos[0], "nombre": datos[1], "apaterno": datos[2], "amaterno": datos[3], "correo": datos[4]}
+            alumno = {'matricula': datos[0], 'nombre': datos[1], 'apaterno': datos[2], 'amaterno': datos[3], 'correo': datos[4]}
             return alumno
         else:
             return None
     except Exception as ex:
-        raise Exception("Error al leer alumno: {}".format(ex))
-
-
+        raise ex
+ 
+ 
+@app.route("/alumnos/<mat>", methods=['GET'])
+def leer_alumno(mat):
+    try:
+        alumno = leer_alumno_bd(mat)
+        if alumno is not None:
+            return jsonify({'alumno': alumno, 'mensaje': 'Alumno encontrado', 'exito': True})
+        else:
+            return jsonify({'alumno': None, 'mensaje': 'Alumno no encontrado', 'exito': False})
+    except Exception as ex:
+        return jsonify({"message": "error {}".format(ex), 'exito': False}), 500
+ 
+@app.route("/alumnosAgr", methods=['POST'])
+def registrar_alumno():
+    try:
+        alumno=leer_alumno_bd(request.json['matricula'])
+        if alumno != None:
+            return jsonify({'mensaje':alumno, 'mensaje': 'Alumno ya existe, no se puede duplicar',
+                            'exito':False})
+        else:
+            cursor= con.connection.cursor()
+            sql="""insert into alumnos (matricula, nombre, apaterno, amaterno, correo)
+            values ('{0}','{1}','{2}','{3}','{4}')""".format(request.json['matricula'],
+                request.json['nombre'],request.json['apaterno'],request.json['amaterno'],
+                request.json['correo'])
+            cursor.execute(sql)
+            con.connection.commit()
+            return jsonify({'mensaje':"Alumno registrado", "exito":True})
+    except Exception as ex:
+        pass
+ 
 def pagina_no_encontrada(error):
-    return "<h1>PAGINA NO ENCONTRADA 404</h1>", 404
-
-if __name__ == "__main__":
+    return "<h1>Pagina no encontrada</h1>", 404
+ 
+if __name__ == '__main__':
     app.config.from_object(config['development'])
-    app.register_error_handler(404, pagina_no_encontrada)
+    app.register_error_handler(400, pagina_no_encontrada)
     app.run(host='0.0.0.0', port=5000)
+ 
